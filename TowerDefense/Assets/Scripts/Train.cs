a793a6251ac2caa54f0c7d23542afe25e2ad15d0
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-05-03 21:53:51 -0300

    Implemented Train Script

---
+++ TowerDefense/Assets/Scripts/Train.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour
+{
+    [SerializeField] private int _life = 100;
+    [SerializeField] private Enemy enemy;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("a ver");
+            _life -= 10;
+            enemy.ArrivedAdTrain();
+        }
+    }
+}
