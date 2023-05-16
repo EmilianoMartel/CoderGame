9413828c72f0c8cbddc71664ceb841dbec7fe6e8
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-05-16 00:44:45 -0300

    Fixed Enemy Enemy follow Way

--- TowerDefense/Assets/Scripts/Enemy.cs
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -11,17 +11,19 @@ public class Enemy : MonoBehaviour
     [SerializeField] private Player player;
     [SerializeField] private Train train;
     [SerializeField] private Animator animator;
-    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float speed = 1f;
     [SerializeField] private int life = 100;
     [SerializeField] private GameObject _pointDirection;
     [SerializeField] private float _rangeTrain = 1f;
-    [SerializeField] private LayerMask  _layerMask;
+    [SerializeField] private LayerMask _layerMask;
 
     //variables cuando muere
     public static event EnemyEvent OnEnemyDeath;
 
     //variables para la direccion
     private Vector3 _direction = new Vector3();
+    [SerializeField] private EnemyWay enemyWay;
+    private int currentIndex;
 
     void FixedUpdate()
     {
@@ -35,36 +37,37 @@ public class Enemy : MonoBehaviour
     private void Start()
     {
         train = GameObject.FindObjectOfType<Train>();
+        enemyWay = GameObject.FindObjectOfType<EnemyWay>();
+        currentIndex = 0;
     }
 
     private void Update()
     {
-        transform.LookAt(train.transform.position);
         Move();
     }
 
     private void Move()
     {
-        _direction = transform.position - train.transform.position;
-
-        //logic animator move
-        if (_direction.magnitude > 0) animator.SetBool("Move", true);
-        else animator.SetBool("Move", false);
-
-        Vector3 directionNormalized = _direction.normalized;
-        this.transform.Translate(directionNormalized * speed * Time.deltaTime);
+        Vector3 destination = enemyWay.GetWaipointPosition(currentIndex);
+        transform.position += (destination - transform.position).normalized * speed * Time.deltaTime;
+        if (Vector3.Distance(transform.position, destination) <= 1)
+        {
+            currentIndex++;
+        }
+        Vector3 foward = (new Vector3(destination.x,transform.position.y,destination.z)-transform.position).normalized;
+        transform.forward = foward;
+        animator.SetBool("Move", true);
     }
 
     public void ArrivedAdTrain()
     {
-        Debug.Log("entraste");
         Destroy(gameObject);
     }
 
     public void Damaged(int damage)
     {
         life -= damage;
-        if(life <= 0)
+        if (life <= 0)
         {
             EnemyKilled(this);
         }
@@ -77,6 +80,7 @@ public class Enemy : MonoBehaviour
         {
             OnEnemyDeath(this);
         }
+        GameManager.INSTANCE.gold += 50;
         GameManager.INSTANCE.FlagEnemyDestroyed(this);
         Destroy(gameObject);
     }
