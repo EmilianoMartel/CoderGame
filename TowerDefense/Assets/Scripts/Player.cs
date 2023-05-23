1ab4674a4777db06526d2d73e540371aed3c3307
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-05-23 20:03:10 -0300

    Implemented ChangeColor Logic

--- TowerDefense/Assets/Scripts/Player.cs
+++ TowerDefense/Assets/Scripts/Player.cs
@@ -4,21 +4,54 @@ using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+
 public class Player : Character
 {
     [SerializeField] private float _sensitivity = 0.5f;
     [SerializeField] private float _rotationY = 0f;
+
+    [SerializeField] private LayerMask m_spawnTowerMask;
+    [SerializeField] private int m_rangeLayer;
+    private SpawnTower spawn;
+
 
     void Update()
     {
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
             PlayerMove();
-        } else
+        }
+        else
         {
-            animator.SetBool("Move",false);
+            animator.SetBool("Move", false);
         }
         Rotation();
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, m_rangeLayer, m_spawnTowerMask))
+        {
+            spawn = hit.transform.GetComponent<SpawnTower>();
+            if (spawn != null)
+            {
+                spawn.ChangeColor(true);
+            }
+        }
+        else
+        {
+            if (spawn != null)
+            {
+                spawn.ChangeColor(false);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.TransformDirection(Vector3.forward));
     }
 
     private void PlayerMove()
@@ -26,7 +59,7 @@ public class Player : Character
         float movementX = Input.GetAxis("Horizontal");
         float movementZ = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(movementX, 0, movementZ);
-        Move(direction);        
+        Move(direction);
     }
 
     private void Rotation()
--- TowerDefense/Assets/Scripts/SpawnTower.cs
+++ TowerDefense/Assets/Scripts/SpawnTower.cs
@@ -3,32 +3,62 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class SpawnTower : MonoBehaviour
+// Define la interfaz que incluye el método para cambiar el color
+public interface IColorChangeable
 {
+    //esto para funcionar en el Awake o Start debes de guardar el color original
+    void ChangeColor(bool isObserved);
+
+}
+public class SpawnTower : MonoBehaviour,IColorChangeable
+{
+
     [SerializeField] GameObject spawnPoint;
+    [SerializeField] GameObject plataform;
     [SerializeField] Tower tower;
     [SerializeField] Player player;
-    private bool active;
+    private bool m_active;
+    private bool m_upgrade=true;
+    private Color m_color;
 
-    // Update is called once per frame
-    void Update()
+
+    private void Awake()
     {
-        Vector3 distance = GameObject.FindGameObjectWithTag("Player").transform.position - transform.position;
-        if (distance.magnitude < 2 && Input.GetKeyDown(KeyCode.E) && active == false && GameManager.INSTANCE.gold >= 100)
-        {
-            Spawn();
-            GameManager.INSTANCE.gold -= 100;
-        }
+        m_color = plataform.GetComponent<MeshRenderer>().material.color;        
     }
 
     private void Spawn()
     {
-        Instantiate(tower, spawnPoint.transform.position, transform.rotation);
-        active= true;
+        tower = Instantiate(tower, spawnPoint.transform.position, transform.rotation);
+        GameManager.INSTANCE.gold -= 100;
+        m_active = true;
+        m_upgrade = false;
     }
 
-    private void Upgrate()
+    private void CanSpawn()
     {
         
+        if (Input.GetKeyUp(KeyCode.E) && m_active == false && GameManager.INSTANCE.gold >= 100)
+        {
+            Spawn();
+            Collider collider = GetComponent<Collider>();
+            if (collider != null)
+            {
+                collider.enabled = false;
+            }
+        }
+    }
+
+    public void ChangeColor(bool isObserved)
+    {
+        if (isObserved)
+        {
+            plataform.GetComponent<MeshRenderer>().material.color = Color.red;
+            CanSpawn();
+        }
+        else
+        {
+            plataform.GetComponent<MeshRenderer>().material.color = m_color;
+        }
     }
 }
